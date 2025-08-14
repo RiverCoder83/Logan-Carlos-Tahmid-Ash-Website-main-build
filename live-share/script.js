2c8591a9fbb3d2db66ae710a5b708962af46cda9
RiverCoder83 <lbenavente101@gmail.com>
2025-08-14 12:15:34 -0700

    Add files via upload

---
+++ live-share/script.js
@@ -0,0 +1,36 @@
+let image1 = document.querySelectorAll("img")[0];
+let image2 = document.querySelectorAll("img")[1];
+let image3 = document.querySelectorAll("img")[2];
+let image4 = document.querySelectorAll("img")[3];
+
+image1.onmouseover = function(){
+    image1.style.width = "205px";
+}
+
+image1.onmouseout = function(){
+    image1.style.width = "200px";
+}
+
+image2.onmouseover = function(){
+    image2.style.width = "205px";
+}
+
+image2.onmouseout = function(){
+    image2.style.width = "200px";
+}
+
+image3.onmouseover = function(){
+    image3.style.width = "205px";
+}
+
+image3.onmouseout = function(){
+    image3.style.width = "200px";
+}
+
+image4.onmouseover = function(){
+    image4.style.width = "205px";
+}
+
+image4.onmouseout = function(){
+    image4.style.width = "200px";
+}
